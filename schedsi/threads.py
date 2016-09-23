z33ky/schedsi

commit c2a6a1b320fdc4e497dd8e044d483f2b85922e78

Track context stack in CPU
Previously the call chain would represent the context stack, starting
with the kernel scheduler, going to the child scheduler, the thread and
the thread would call execute_thread on the core.
Now the CPU tracks the call stack, so it is in control of returning
execution to the parent. Also the core calls execute() on a thread, not
the other way around.

The new approach is detailed in HACKING.

This is an invasive change, touching all schedsi files, but with this
change we can easily implement multi-core and external interrupts.
It also makes more sense to have the CPU at the bottom of the call-chain
and not the top.

This does complicate thread implementation slightly and schedulers
significantly, because they are interruptible in a sense. They are at
fixed points, but in preparation for modeling scheduler execution times
and multi-core scheduling without static partitioning the schedulers
need to deal with some synchronization.
This is implemented via RCU. As a bonus, since the RCU emulation is
thread-safe, the schedulers should also be thread-safe now.

The binary log also emulates the context stack of the CPU when replaying.
To achieve this the logs gained an init_core event, which the binary log
uses to obtain the initial context stack containing the kernel scheduler.
Direction recording has changed so that replaying the stack is possible.

With this commit, threads becoming ready are added to the ready queue of
the scheduler as they become ready and removed from it when they are
not, so the order schedulers select threads may change.

schedsi/threads.py
@@ -2,6 +2,7 @@
 """Thread classes."""
 
 import sys
+import threading
 
 class Thread:
     """The basic thread class.
@@ -11,9 +12,10 @@ class Thread:
         * a locally unique thread id
         * ready time (-1 if finished)
         * remaining workload (-1 if infinite)
-        * last deschedule time (-1 if never)
+        * finished time (-1 if never)
         * total runtime
         * total waittime
+        * a lock indicating whether this thread is currently active
     """
 
     def __init__(self, module, tid, ready_time, units):
@@ -22,60 +24,88 @@ class Thread:
         self.tid = tid
         self.ready_time = ready_time
         self.remaining = units
-        self.last_deschedule = -1
+        self.finished_time = -1
         self.total_run_time = 0
         self.total_wait_time = 0
+        self.is_running = threading.Lock()
 
-    def execute(self, cpu):
+    def execute(self):
         """Simulate execution.
 
         The thread will run for as long as it can.
 
-        The time spent executing is returned.
+        Yields the amount of time it wants to execute,
+        -1 for infinite and 0 to indicate the desire to yield,
+        or a thread to switch to. None for no-op.
+        Consumes the current time.
         """
-        return self._execute(cpu, -1)
+        locked = self.is_running.acquire(False)
+        assert locked
 
-    def _execute(self, cpu, run_time):
+        current_time = yield
+        while True:
+            current_time = yield from self._execute(current_time, -1)
+
+    def _execute(self, current_time, run_time):
         """Simulate execution.
 
-        The thread will run for as long as it can.
+        Update some state.
+        Yields `run_time` respecting :attr:`remaining`, so it won't
+        yield more than that.
 
-        The time spent executing is returned.
+        Returns the next current time or None if :attr:`remaining` is 0.
         """
-        assert self.ready_time != -1 and self.ready_time <= cpu.status.current_time
-        assert self.remaining != 0
+        assert self.ready_time != -1 and self.ready_time <= current_time
+        assert self.remaining != 0 and self.finished_time == -1
+        assert not self.is_running.acquire(False)
+        self.total_wait_time += current_time - self.ready_time
 
-        if run_time == -1 or (run_time > self.remaining and self.remaining != -1):
+        if run_time == -1:
             run_time = self.remaining
-        assert run_time <= self.remaining or self.remaining == -1
+        else:
+            assert run_time <= self.remaining or self.remaining == -1
 
-        run_time = cpu.crunch(self, run_time)
-        if run_time == 0:
-            return 0
+        current_time = yield run_time
 
-        current_time = cpu.status.current_time
+        if self.remaining == 0:
+            yield 0
+            return
 
-        self._update_timing_stats(current_time - run_time, run_time, current_time)
+        return current_time
+
+    def run(self, _current_time, run_time):
+        """Update runtime state.
+
+        This should be called while the thread is active.
+        """
+        assert not self.is_running.acquire(False)
+        self.total_run_time += run_time
 
         if self.remaining != -1:
+            assert self.remaining >= run_time
             self.remaining -= run_time
-            assert self.remaining >= 0
+
+    def finish(self, current_time):
+        """Become inactive.
+
+        This should be called when the thread becomes inactive.
+        """
+        assert not self.is_running.acquire(False)
+        self._finish(current_time)
+
+        self.is_running.release()
+
+    def _finish(self, current_time):
+        assert not self.is_running.acquire(False)
 
         if self.remaining == 0:
             #the job was completed within the slice
             #never start again
             self.ready_time = -1
-        else:
+            self.finished_time = current_time
+        elif self.remaining != -1:
             #not enough time to complete the job
             self.ready_time = current_time
-
-        return run_time
-
-    def _update_timing_stats(self, start_time, run_time, current_time):
-        """Update total_wait_time, total_run_time, last_deschedule."""
-        self.total_wait_time += start_time - max(self.last_deschedule, self.ready_time)
-        self.total_run_time += run_time
-        self.last_deschedule = current_time
 
 class SchedulerThread(Thread):
     """A thread representing a VCPU for a child.
@@ -88,17 +118,23 @@ class SchedulerThread(Thread):
         super().__init__(scheduler.module, tid, 0, -1)
         self._scheduler = scheduler
 
-    def execute(self, cpu):
+    def execute(self):
         """Simulate execution.
 
         Simply forward to the scheduler.
 
         See :meth:`Thread.execute`.
         """
-        run_time = self._scheduler.schedule(cpu) # pylint: disable=not-callable
-        current_time = cpu.status.current_time
-        self._update_timing_stats(current_time - run_time, run_time, current_time)
-        return run_time
+        locked = self.is_running.acquire(False)
+        assert locked
+
+        scheduler = self._scheduler.schedule()
+        thing = next(scheduler)
+        assert thing is None
+        current_time = yield
+        while True:
+            next(super()._execute(current_time, -1))
+            current_time = yield scheduler.send(current_time)
 
     def add_threads(self, new_threads):
         """Add threads to scheduler."""
@@ -113,30 +149,28 @@ class VCPUThread(Thread):
     def __init__(self, module, tid, child):
         """Create a :class:`VCPUThread`."""
         if child.parent != module:
-            print(module.name, "is adding a VCPUThread for", child,
-                  "although it is not a direct descendant.", file=sys.stderr)
-        child_thread = child.register_vcpu(self)
-        super().__init__(module, tid, child_thread.ready_time, child_thread.remaining)
-        self._thread = child_thread
+            print(module.name, "is adding a VCPUThread for", child.name,
+                  "although it is not a direct descendant.")
+        super().__init__(module, tid, None, None)
+        self._thread = child.register_vcpu(self)
         if not isinstance(self._thread, SchedulerThread):
             print("VCPUThread expected a SchedulerThread, got", type(self._thread).__name__, ".",
                   file=sys.stderr)
 
-    def execute(self, cpu):
+    def execute(self):
         """Simulate execution.
 
         Switch context and forward to child thread.
 
         See :meth:`Thread.execute`.
         """
-        run_time = cpu.switch_module(self._thread.module)
-        run_time += self._thread.execute(cpu)
-        run_time += cpu.switch_module(self.module)
+        locked = self.is_running.acquire(False)
+        assert locked
 
-        current_time = cpu.status.current_time
-        self._update_timing_stats(current_time - run_time, run_time, current_time)
-
-        return run_time
+        current_time = yield
+        while True:
+            next(super()._execute(current_time, -1))
+            current_time = yield self._thread
 
     def __getattribute__(self, key):
         """ready_time and remaining should be taken from the :class:`SchedulerThread`."""
@@ -155,29 +189,59 @@ class PeriodicWorkThread(Thread):
         self.original_ready_time = self.ready_time
         self.period = period
         self.burst = burst
+        self.current_burst_left = None
+        self.burst_started = None
+
+    def _get_quota(self, current_time):
+        """Calculate the quote at `current_time`.
+
+        Won't return more than :attr:`remaining`.
+        """
+        activations = int((current_time - self.original_ready_time) / self.period) + 1
+        quota = activations * self.burst
+        if self.remaining != -1:
+            quota = min(self.remaining + self.total_run_time, quota)
+        return quota
 
     #will run as long as the summed up bursts require
-    def execute(self, cpu):
+    def execute(self):
         """Simulate execution.
 
         See :meth:`Thread.execute`.
         """
-        #how often we wanted to be executed (including this one)
-        activations = int((cpu.status.current_time - self.original_ready_time) / self.period) + 1
-        quota = activations * self.burst
+        locked = self.is_running.acquire(False)
+        assert locked
 
-        if quota < 0:
-            raise RuntimeError('Scheduled too eagerly')
-        quota_left = quota - self.total_run_time
-        if quota_left < 0:
-            raise RuntimeError('Executed too much')
+        current_time = yield
+        while True:
+            quota = self._get_quota(current_time)
+            if quota < 0:
+                raise RuntimeError('Scheduled too eagerly')
+            quota_left = quota - self.total_run_time
+            if quota_left < 0:
+                raise RuntimeError('Executed too much')
+            quota_plus = self._get_quota(current_time + quota_left) - self.total_run_time
+            self.burst_started = current_time - self.original_ready_time
+            #TODO: be smarter
+            while quota_plus > quota_left:
+                quota_left = quota_plus
+                quota_after = self._get_quota(current_time + quota_left) - self.total_run_time
+                self.burst_started += self.period
+            self.current_burst_left = quota_left
 
-        run_time = super()._execute(cpu, quota_left)
+            current_time = yield from super()._execute(current_time, quota_left)
 
-        assert run_time <= quota_left
-        if self.remaining > 0 or self.remaining == -1:
-            if quota_left == run_time:
-                #set ready_time to next burst arrival
-                self.ready_time = self.original_ready_time + activations * self.period
+    def run(self, current_time, run_time):
+        """See :meth:`Thread.run`."""
+        super().run(current_time, run_time)
+        assert self.current_burst_left >= run_time
+        self.current_burst_left -= run_time
 
-        return run_time
+    def _finish(self, current_time):
+        super()._finish(current_time)
+        #set ready_time to next burst arrival, if required
+        if (self.remaining > 0 or self.remaining == -1) \
+           and self.current_burst_left == 0:
+            self.ready_time = self.burst_started + self.period
+            self.burst_started = None
+        self.current_burst_left = None

HACKING
@@ -1,42 +1,51 @@
 The simulation is kicked off in `World.step()` (´schedsi/world.py´).
-From there, the scheduler of the main `Module` (kernel) is invoked.
+From there, `execute()` of each `Core` is invoked, which will proceed one timer slice.
 
-The execution and scheduling functions all return the time they ran.
+The `Core` has a stack of contexts, where the first element of the stack in the kernel scheduler
+and each successive element is either a thread from the same module or the scheduler thread of a child module.
+It is imaginable that child threads run directly on the parent, but this is currently not used.
 
-The `Core` (`schedsi/cpu.py`) is the root for the timing information.
-A `Thread` can request to run a certain time by passing the requested time to the `crunch()` function.
-The `Core` will then look how much of the current time slice remains and lets time pass accordingly.
-If the requested time exceeds the time slice a flag will be set indicating that an interrupt must be delivered.
-The `Core` will deny any execution until the `finish_step` function is called, indicating that the kernel is handling the timer interrupt.
+The scheduler is a coroutine that can yield a number > 0 to indicate that it's using some processor time. -1 is also valid and stands for execution as long as possible (the remaining time slice).
+Yielding a value of 0 means that the scheduler currently has no `Thread`s to schedule, meaning that the `Core` will return execution to the parent,
+or if the kernel scheduler yield, the `Core` will idle the remaining time slice.
+The scheduler may also yield a `Thread`, which the `Core` interprets as a request to switch context to that `Thread`, so it's context is pushed on the stack.
 
-The `Core` is split into three parts:
+A `Thread` may also yield the same values as a scheduler. In fact, schedulers are represented as `Thread`s via the `SchedulerThread` class.
+
+Whenever the `Core` receives the request to spend time executing the `Thread` it will check how long it can run and call the `run()` function of the thread and pass the current time and the time it runs for.
+This allows the `Thread` to gather how long it has actually run.
+When the `Core` pops `Thread`s off the stack, it will call their `finish()` function, passing in the current time.
+
+The `Core` is split into two parts:
 	* `Core`, which itself contains mostly invariant information, like the `uid`
 	* `_CoreStatus`, which contains variable information, such as the current `time_slice` and `current_time`
-	* `_CoreContext`, which keeps track of the current context
-The `_CoreContext` would correspond to the loaded registers and page tables.
-Within the simulator this is represented by storing a reference to the currently active `Module` (process) and `Thread`.
+There's also the `_CoreContext` class, which contains an operation context.
+On a real machine it would contain a register dump, in the simulator this is represented by storing a reference to the `Thread`.
 Switching context has a cost and this is also simulated, although currently only for switching between `Module`s.
 
 The `Core` and the `Thread` record various statistics (mostly on timing), which are currently not pulled out and evaluated.
 
-If the call-chain returns to the `World` without the time slice being used up, the CPU will idle for the remaining slice.
+When a time slice is used up, and the next one begins, the `Core` will pop all threads from the context stack (calling `finish() on them`) with exception of the first, the kernel scheduler, where execution will resume next.
 
-All events are logged in a logger class.
-These logger classes are not loggers in the sense that they expect a string and store it somewhere, instead there is a function for each relevant event and the log will aggregate the relevant information for that event.
-I expect the main logger will be the binary logger (`schedsi/binarylog.py`, `class BinaryLog`), since it has a `replay()` function.
+All actions of the `Core` are logged in a logger class.
+These logger classes are not loggers in the sense that they expect a string and store it somewhere, instead there is a function for each relevant event and the log will aggregate the relevant information for that event to store or present it how it sees fit.
+I expect the main logger will be the binary logger (`schedsi/binarylog.py`, `class BinaryLog`), since it has a `replay()` function to replay a binary log file to another logger (see `replay.py').
 Replaying is a bit of a hack and relies on Pythons duck-typing to work via "emulation classes" that pretend to be the real deal.
 If the emulation classes prove to be insufficient we might have to implement a "replay scheduler" and (probably) also store the hierarchy in the log file.
 The other loggers are the `TextLog` (`schedsi/textlog.py`) and the `GraphLog` (`schedsi/graphlog.py`). Both work fine with `BinaryLog.replay()`.
 The `TextLog` is the easiest to understand, since it simply pulls out some information from the `Core` it received the event from and formats it to a string.
 The `BinaryLog` is less straight-forward; It aggregates 'relevant' information in a dictionary of primitive types and writes that to a MessagePack stream.
-The `GraphLog` also needs to keep track of some state for drawing, for instance what the current hierarchy depth is.
+The `GraphLog` also needs to keep track of some state for drawing, for instance what the current hierarchy depth is. Since switching the way `Core`s execute this could also be pulled from the context stack, but before that change this was not possible otherwise.
 
-The only schedulers implemented at the moment is a preemptive round robin scheduler (`schedsi/round_robin.py`) and a single-thread scheduler serving as a base class (`schedsi/scheduler.py`).
+The schedulers implemented at the moment is a preemptive round robin scheduler (`schedsi/round_robin.py`), a shorted job first scheduler (`schedsi/shortest_job_first.py`) and a single-thread scheduler serving as a base class (`schedsi/scheduler.py`).
+The SJF scheduler peeks directly into the `Thread`s to find their remaining time, which on a real system is not likely to be available information.
 
 Process hierarchies are built by creating the root of the hierarchy (the kernel) and adding further modules onto it.
 The `tests/simple_example.py` should give you a good idea on how it works.
 
 Each `Module` (`schedsi/module.py`) has some threads. It has at least one, the `SchedulerThread` (`schedsi/threads.py`), which forwards execution to the scheduler.
-For the kernel, the `schedule()` function is invoked from `World.step()`. For each child `Module`, the kernel should provide a `VCPUThread`, which is used to `execute()` the children's `SchedulerThread`.
-There are two threads to simulate load, the bare `Thread`, which will just continuously execute until its workload is finisheed (`Thread.remaining`) and `PeriodicWorkThread`, which describes its workload in regular appearing bursts.
+For each child `Module`, the parent should provide a `VCPUThread`, which is used to `execute()` the children's `SchedulerThread`. If `Module`s decide to not employ their own scheduler for some threads, they can be added directly to the parent module.
+There are two thread classes to simulate load:
+	* the bare `Thread`, which will just continuously execute until its workload is finisheed (`Thread.remaining`)
+	* the `PeriodicWorkThread`, which describes its workload in regular appearing bursts
 Furthermore, each thread can have a `start_time` at which it will begin execution.

example/kernel.py
@@ -51,7 +51,7 @@ def main():
 
         #Create and run the world.
         the_world = world.World(1, 10, KERNEL, binary_log)
-        while the_world.step() < 400:
+        while the_world.step() <= 400:
             pass
 
 if __name__ == '__main__':

commit 894af4f4db9a68a517526c102ce371279b420711

Fix error printing to stdout
Introduced in c2a6a1b320fdc4e497dd8e044d483f2b85922e78.

schedsi/threads.py
@@ -155,7 +155,7 @@ class VCPUThread(Thread):
         """Create a :class:`VCPUThread`."""
         if child.parent != module:
             print(module.name, "is adding a VCPUThread for", child.name,
-                  "although it is not a direct descendant.")
+                  "although it is not a direct descendant.", file=sys.stderr)
         super().__init__(module, *args, **kwargs, ready_time=None, units=None)
         self._thread = child.register_vcpu(self)
         if not isinstance(self._thread, SchedulerThread):