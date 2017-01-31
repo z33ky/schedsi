z33ky/schedsi

commit 4678febb9c7d337fde5ec149f2175ccb33eb1bea

Change from periodic to one-shot scheduling timer
The kernel scheduler now sets the next timer when it schedules a
thread.
The timer_quantum parameter of World and Core are removed in favor of a
time_slice parameter for Scheduler, who's now responsible for setting
the timer.

schedsi/cpu.py
@@ -37,7 +37,6 @@ class _Status:
         """Create a :class:`_Status`."""
         self.cpu = cpu
         self.chain = chain
-        self.chain.set_timer(cpu.timer_quantum)
         self.prev_chain = None
         self.current_time = 0
         self.stats = _TimeStats()
@@ -62,7 +61,7 @@ class _Status:
         elif time > timeout or time == -1:
             time = max(0, timeout)
 
-        assert time > 0
+        assert time > 0 or time == 0 and timeout <= 0
         return time
 
     def _update_time(self, time):
@@ -73,7 +72,7 @@ class _Status:
         assert time > 0
         timeout = self.chain.next_timeout
         if not timeout is None:
-            assert time <= timeout
+            assert time <= timeout or timeout <= 0
 
         self.current_time += time
         self.chain.elapse(time)
@@ -87,7 +86,7 @@ class _Status:
     def _timer_interrupt(self):
         """Call when :attr:`chain.next_timeout` arrives.
 
-        Sets up a new timer and jumps back to the kernel.
+        Resets the timer and jumps back to the kernel.
         """
         assert self.chain.next_timeout <= 0
 
@@ -96,17 +95,16 @@ class _Status:
 
         idx = self.chain.find_elapsed_timer()
         assert idx == 0
-        self.chain.set_timer(self.cpu.timer_quantum, idx)
         new_top = self.chain.thread_at(idx)
 
         time = self._context_switch(new_top)
-        assert self.chain.next_timeout > 0, "CTXSW_COST is too big or timer_quantum too small"
         self.stats.timer_delay += time
         new_top.run_ctxsw(self.current_time, time)
 
         prev_chain = self.chain.split(idx + 1)
         prev_chain.finish(self.current_time)
 
+        self.chain.set_timer(None)
         assert len(self.chain) == 1
         self.chain.current_context.restart(self.current_time)
 
@@ -175,7 +173,7 @@ class _Status:
         #For multi-core emulation this should become a coroutine.
         #It should yield whenever current_time is updated.
 
-        if self.chain.next_timeout <= 0:
+        if not self.chain.next_timeout is None and self.chain.next_timeout <= 0:
             self._timer_interrupt()
             return
 
@@ -186,7 +184,7 @@ class _Status:
                 continue
             elif next_step.rtype == cpurequest.Type.execute:
                 time = self._calc_runtime(next_step.thing)
-                assert time > 0, time <= next_step
+                assert time > 0 and time <= next_step.thing
                 self.cpu.log.thread_execute(self.cpu, time)
                 self._update_time(time)
                 self.stats.crunch_time += time
@@ -200,6 +198,10 @@ class _Status:
                 assert len(next_step.thing) == 1
                 chain = context.Chain.from_thread(next_step.thing.bottom)
                 self._append_chain(chain)
+            elif next_step.rtype == cpurequest.Type.timer:
+                if self.chain.top.module != self.cpu.kernel:
+                    raise RuntimeError('Received timer request from non-kernel scheduler.')
+                self.chain.set_timer(next_step.thing)
             else:
                 assert False
             break
@@ -216,10 +218,9 @@ class Core:
     The values are not expected to change much during operation.
     """
 
-    def __init__(self, uid, timer_quantum, init_thread, log):
+    def __init__(self, uid, init_thread, log):
         """Create a :class:`Core`."""
         self.uid = uid
-        self.timer_quantum = timer_quantum
 
         self.log = log
 
@@ -239,3 +240,8 @@ class Core:
         stats = self.status.stats.__dict__.copy()
         stats.update(self.status.ctxsw_stats.__dict__)
         return stats
+
+    @property
+    def kernel(self):
+        """The Kernel module."""
+        return self.status.chain.bottom.module

example/kernel.py
@@ -12,7 +12,7 @@ from schedsi import binarylog, hierarchy_builder, schedulers, threads, world
 #Create a hierarchy of a kernel, a child module and two grand-children.
 #this is the same as tests/simple_hierarchy.py
 #pylint: disable=duplicate-code
-KERNEL = hierarchy_builder.ModuleBuilder(scheduler=schedulers.RoundRobin)
+KERNEL = hierarchy_builder.ModuleBuilder(scheduler=schedulers.RoundRobin.builder(time_slice=10))
 TOP_MODULE = KERNEL.add_module(scheduler=schedulers.RoundRobin)
 BOTTOM_MODULE_A = TOP_MODULE.add_module(scheduler=schedulers.RoundRobin)
 BOTTOM_MODULE_B = TOP_MODULE.add_module(scheduler=schedulers.SJF)
@@ -42,7 +42,7 @@ def main():
         binary_log = binarylog.BinaryLog(log_file)
 
         #Create and run the world.
-        the_world = world.World(1, 10, KERNEL.module, binary_log)
+        the_world = world.World(1, KERNEL.module, binary_log)
         while the_world.step() <= 400:
             pass
 

schedsi/context.py
@@ -155,7 +155,6 @@ class Chain:
             #no time to count down then
             return
         assert self.contexts
-        assert self.next_timeout is None or self.next_timeout >= time
 
         for ctx in self.contexts:
             if not ctx.timeout is None:

schedsi/cpurequest.py
@@ -4,7 +4,7 @@
 import enum
 from schedsi import context
 
-Type = enum.Enum('Type', ['current_time', 'resume_chain', 'idle', 'execute'])
+Type = enum.Enum('Type', ['current_time', 'resume_chain', 'idle', 'execute', 'timer'])
 
 class Request:
     """A request to the CPU."""
@@ -19,6 +19,8 @@ class Request:
             assert thing is None
         elif rtype == Type.execute:
             assert thing > 0 or thing == -1
+        elif rtype == Type.timer:
+            assert thing > 0
         else:
             assert False, "Unknown Type"
         self.rtype = rtype
@@ -46,3 +48,11 @@ class Request:
     def execute(cls, amount):
         """Create a :class:`Request` to spend some time executing."""
         return cls(Type.execute, amount)
+
+    @classmethod
+    def timer(cls, time):
+        """Create a :class:`Request` to set a timer for the current context."""
+        #TODO: allow to specify how much sooner a timer may elapse
+        #      this is to reduce frequent ctxswes if resumed chains have
+        #      timers that will elapse very soon
+        return cls(Type.timer, time)

schedsi/multilevel_feedback_queue.py
@@ -38,14 +38,14 @@ class MLFQData(scheduler.SchedulerData): # pylint: disable=too-few-public-method
 class MLFQ(scheduler.Scheduler):
     """Multi-level feedback queue scheduler."""
 
-    def __init__(self, module, *, levels=8, priority_boost_time):
+    def __init__(self, module, *, levels=8, priority_boost_time, **kwargs):
         """Create a class:`MLFQ`."""
         assert levels > 0
         if not priority_boost_time is None:
             #priority boost has no effect with 1 queue
             assert levels != 1
             assert priority_boost_time >= 0
-        super().__init__(module, MLFQData(levels, priority_boost_time))
+        super().__init__(module, MLFQData(levels, priority_boost_time), **kwargs)
         self.prio_boost_time = priority_boost_time
         if priority_boost_time is None:
             self._priority_boost = self._no_priority_boost

schedsi/round_robin.py
@@ -10,6 +10,6 @@ class RoundRobin(multilevel_feedback_queue.MLFQ):
     this scheduler is simply implemented as MLFQ with 1 queue.
     """
 
-    def __init__(self, module):
+    def __init__(self, module, **kwargs):
         """Create a :class:`RoundRobin` scheduler."""
-        super().__init__(module, levels=1, priority_boost_time=None)
+        super().__init__(module, levels=1, priority_boost_time=None, **kwargs)

schedsi/scheduler.py
@@ -26,16 +26,34 @@ class Scheduler:
     Has a :obj:`list` of :class:`context.Chains <schedsi.context.Chain>`.
     """
 
-    def __init__(self, module, rcu_storage=None):
+    def __init__(self, module, rcu_storage=None, *, time_slice=None):
         """Create a :class:`Scheduler`.
 
         Optionally takes a `rcu_storage` for which to create the :attr:`_rcu` for.
         It should be a subclass of :class:`SchedulerData`.
+
+        The `time_slice` is for the kernel scheduler to set.
         """
         if rcu_storage is None:
             rcu_storage = SchedulerData()
         self._rcu = rcu.RCU(rcu_storage)
         self.module = module
+        self.time_slice = time_slice
+        #kernel and only kernel must set time_slice
+        assert (self.module.parent is None) != (self.time_slice is None)
+
+    @classmethod
+    def builder(cls, *args, **kwargs):
+        """Make a creator of :class:`Scheduler`.
+
+        Returns a function taking a single argument:
+        the :class:`Module` of the scheduler to create.
+        `args` and `kwargs` are then also forwarded to :meth:`__init__`.
+        """
+        def make(module):
+            """The creator function to be returned."""
+            return cls(module, *args, **kwargs)
+        return make
 
     def num_threads(self):
         """Return total number of threads.
@@ -169,6 +187,9 @@ class Scheduler:
         if idx == -1:
             yield cpurequest.Request.idle()
             return
+
+        if not self.time_slice is None:
+            yield cpurequest.Request.timer(self.time_slice)
 
         rcu_copy.data.ready_chains[idx] = \
             yield cpurequest.Request.resume_chain(rcu_copy.data.ready_chains[idx])

schedsi/shortest_job_first.py
@@ -7,9 +7,9 @@ from schedsi import scheduler
 class SJF(scheduler.Scheduler):
     """Shortest job first scheduler."""
 
-    def __init__(self, module):
+    def __init__(self, module, **kwargs):
         """Create a :class:`SJF` scheduler."""
-        super().__init__(module)
+        super().__init__(module, **kwargs)
 
     def _update_ready_chains(self, time, rcu_data):
         """See :meth:`Scheduler._update_ready_chains`.

commit d4dbee7c7867e88cdf61d98bea43f584501c788e

Allow infinite execution requests again
This was accidentally broken in
4678febb9c7d337fde5ec149f2175ccb33eb1bea.

schedsi/cpu.py
@@ -193,7 +193,8 @@ class _Status:
             return False
         elif request.rtype == cpurequest.Type.execute:
             time = self._calc_runtime(request.thing)
-            assert time > 0 and time <= request.thing
+            assert time > 0
+            assert time <= request.thing or request.thing == -1
             self.cpu.log.thread_execute(self.cpu, time)
             self._update_time(time)
             self.stats.crunch_time += time