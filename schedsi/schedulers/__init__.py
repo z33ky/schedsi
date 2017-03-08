8d6a08142602c928c05a97d7427d879d7697d76d
Alexander 'z33ky' Hirsch <1zeeky@gmail.com>
2017-03-08 13:59:59 +0100

    Add First Come First Serve scheduler

--- schedsi/schedulers/__init__.py
+++ schedsi/schedulers/__init__.py
@@ -6,3 +6,4 @@ from .multilevel_feedback_queue import MLFQ
 from .round_robin import RoundRobin
 from .scheduler import Scheduler as Single
 from .shortest_job_first import SJF
+from .first_come_first_serve import FCFS
---
+++ schedsi/schedulers/first_come_first_serve.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Defines a first come first serve scheduler."""
+
+from schedsi.schedulers import scheduler
+
+
+class FCFS(scheduler.Scheduler):
+    """First come first serve scheduler."""
+
+    def __init__(self, module, *, time_slice=None, **kwargs):
+        """Create a :class:`SJF` scheduler."""
+        if time_slice is not None:
+            raise RuntimeError('FCFS does not use a time-slice.')
+        super().__init__(module, **kwargs)
+
+    def _sched_loop(self, rcu_copy, _last_chain_queue, _last_chain_idx):
+        """Schedule the next :class:`~schedsi.threads.Thread`.
+
+        See :meth:`~schedsi.scheduler.Scheduler._sched_loop`.
+        """
+        idx = 0
+        if not rcu_copy.data.ready_chains:
+            idx = -1
+        return idx, self.time_slice
+        # needs to be a coroutine
+        yield  # pylint: disable=unreachable
