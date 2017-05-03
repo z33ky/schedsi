fd5171d41a46418f5a04030298c2380c84bbef2f
Alexander 'z33ky' Hirsch <1zeeky@gmail.com>
2017-05-03 15:46:50 +0200

    Add missing examples to tests/examples.py

--- tests/examples.py
+++ tests/examples.py
@@ -26,8 +26,20 @@ class TestExample(unittest.TestCase):
         self.example('penalty_scheduler')
 
     def test_penalty_scheduler(self):  # pylint: disable=no-self-use
+        """Test that the penalty-cfs example can run."""
+        self.example('penalty_cfs')
+
+    def test_maximizing_scheduler(self):  # pylint: disable=no-self-use
         """Test that the maximizing-scheduler example can run."""
         self.example('maximizing_scheduler')
+
+    def test_cfs(self):  # pylint: disable=no-self-use
+        """Test that the cfs example can run."""
+        self.example('cfs')
+
+    def test_fixed_cfs(self):  # pylint: disable=no-self-use
+        """Test that the fixed-cfs example can run."""
+        self.example('fixed_cfs')
 
 
 if __name__ == '__main__':
