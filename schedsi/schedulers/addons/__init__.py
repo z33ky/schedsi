824f2096bf128a0e28fb07e9f41a04ad9649d19e
Alexander 'z33ky' Hirsch <1zeeky@gmail.com>
2017-05-09 17:40:54 +0200

    Add PenalizingMaximizer addon
    Combines the Penalizer and TimeSliceMaxer.

--- schedsi/schedulers/addons/__init__.py
+++ schedsi/schedulers/addons/__init__.py
@@ -3,4 +3,5 @@
 from .time_slice_fixer import TimeSliceFixer
 from .time_slice_maxer import TimeSliceMaxer
 from .penalizer import Penalizer
+from .penalizer_maxer import PenalizingMaximizer  # MaximizingPenalizer
 #from .penalty_tracker import PenaltyTracker
